fac99d89627a9995261ded16707717da0234d6e2
Osquera <antonmosquerastorgaard@gmail.com>
2025-10-19 22:59:19 +0200

    Made an implementation of kfold corss validation

--- Project_2/run_on_gpu.sh
+++ Project_2/run_on_gpu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#BSUB -J video_classification
+#BSUB -J video_cv_all
 #BSUB -q c02516
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 02:00
-#BSUB -R "rusage[mem=2GB]"
+#BSUB -R "rusage[mem=4GB]"
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
 #BSUB -o logs/%J.out
@@ -13,13 +13,15 @@
 mkdir -p logs
 
 # Default model parameters
-MODEL="3D_CNN"
-NUM_EPOCHS=5
+MODEL="all"
+NUM_EPOCHS=10
 BATCH_SIZE=8
 IMAGE_SIZE=64
 OUTPUT_DIR="./outputs"
 SAVE_MODEL=""  # Empty means don't save
 ROOT_DIR="/dtu/datasets1/02516/ufc10"
+CROSS_VALIDATE="--cross_validate"
+N_FOLDS=5
 # No leakage dir: /dtu/datasets1/02516/ucf101_noleakage
 
 # Parse command line arguments
@@ -53,6 +55,14 @@ while [[ $# -gt 0 ]]; do
       ROOT_DIR="$2"
       shift 2
       ;;
+    --n_folds)
+      N_FOLDS="$2"
+      shift 2
+      ;;
+    --no_cv)
+      CROSS_VALIDATE=""
+      shift
+      ;;
     *)
       echo "Unknown option: $1"
       exit 1
@@ -79,6 +89,11 @@ echo "Image size: $IMAGE_SIZE"
 echo "Output directory: $OUTPUT_DIR"
 echo "Save model: ${SAVE_MODEL:-No}"
 echo "Dataset path: $ROOT_DIR"
+if [ -n "$CROSS_VALIDATE" ]; then
+  echo "Cross-validation: Yes, with $N_FOLDS folds"
+else
+  echo "Cross-validation: No"
+fi
 
 # Run the main.py script with provided arguments
 # Assuming main.py is in the current directory (Project_2)
@@ -89,9 +104,13 @@ python Project_2/main.py \
   --image_size $IMAGE_SIZE \
   --output_dir $OUTPUT_DIR \
   --root_dir $ROOT_DIR \
+  --n_folds $N_FOLDS \
+  $CROSS_VALIDATE \
   $SAVE_MODEL
 
 # Command to run (example):
-# ./run_on_gpu.sh --model 2D_CNN_aggr --num_epochs 10 --save_model
-# or submit to queue:
+# ./run_on_gpu.sh --model 2D_CNN_aggr --num_epochs 15 --save_model
+# To run without cross-validation:
+# ./run_on_gpu.sh --no_cv --model 2D_CNN_aggr
+# To submit to queue:
 # bsub < run_on_gpu.sh
